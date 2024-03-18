dbc9039957e3685c38eec80466615860f5ed73a0
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-03-19 02:30:23 +0600

    winston logger added with date

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { logger, errorLogger } from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Connected to MongoDB')
+    logger.info('Connected to MongoDB')
     app.listen(config.port, () => {
-      console.log(`University listening on port ${config.port}`)
+      logger.info(`University listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log(err)
+    errorLogger.error(`Failed to connect database ${err}`)
   }
 }
 
