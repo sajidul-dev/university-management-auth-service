6476d9d8987f414ba4d9249d1f88784a8d21ab05
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-10-30 17:24:08 +0600

    Academic semester create api implemented

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import { AcademicSemesterService } from './academicSemester.service';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
+
+const createSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { ...academicSemesterData } = req.body;
+    const result =
+      await AcademicSemesterService.createSemester(academicSemesterData);
+    next();
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Academic semester is created successfully!',
+      data: result,
+    });
+  },
+);
+
+export const AcademicSemesterController = { createSemester };
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,22 +1,22 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { UserService } from './user.service';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
+const createUser: RequestHandler = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req.body;
     const result = await UserService.createUser(user);
-    res.status(200).json({
+    next();
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: 'User created successfully!',
       data: result,
     });
-  } catch (err) {
-    // res.status(400).json({
-    //   success: false,
-    //   message: 'Failed to create user',
-    // })
-    next(err);
-  }
-};
+  },
+);
 
 export const UserController = { createUser };
